47869ab19bdf776538c0bfc4899098d96f3b736d
izzyfizzyy <izabela.arciszewska0@gmail.com>
2025-09-24 21:34:40 +0200

    added class for game core and adjusted welcome class

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import sys
+
 from game_core2 import GameCoreCopy
 
 
@@ -5,35 +7,38 @@ class App:
     def __init__(self):
         pass
 
-    def _display_welcome_screen(self):
+    @staticmethod
+    def _display_welcome_screen():
         print("Welcome to tic-tac-toe")
         print("*********")
-        print(" |TIC|\n |TAC|\n |TOC|")
+        print(" |TIC|   |   |\n |   |TAC|   |\n |   |   |TOC|")
         print("Select:")
         print("(N) New Game\n(O) Options\n(H) Help\n(Q) Quit")
 
-    def _play_board(self):
-        print("*********")
-        print("Playboard:")
-        print("     1   2   3   \n 1 |   |   |   |\n 2 |   |   |   |\n 3 |   |   |   |")
-        print("Choose your player (1/2) and put coordinates for example 23 meaning 2 row and 3 column")
-        print("Your input should look like this: X enter 2 enter 3 enter to achieve:")
+    @staticmethod
+    def _display_rules():
+        print(
+            "Choose your player (1 or 2) and put coordinates in 3x3 matrix. For example 123 meaning player 1 choose 2 row and 3 column:")
         print("     1   2   3   \n 1 |   |   |   |\n 2 |   |   | X |\n 3 |   |   |   |")
 
-    def _display_help_screen(self):
+    @staticmethod
+    def _display_help_screen():
         print("Tic-tac-toe is a two-player game played on a 3x3 grid.\n" +
               "Players take turns placing their symbols (X or O) in empty squares, " +
               "aiming to be the first to get three in a row (horizontally, vertically, or diagonally).\n" +
               "If all squares are filled and no one has three in a row, the game is a draw")
 
+    def _display_options(self):
+        pass
+
     def run(self):
         self._display_welcome_screen()
-        self._play_board()
         game = GameCoreCopy()  # tworzenie instancji klasy GameCoreCopy
 
         while True:
             user_input = input()
             if user_input.lower() == "n":
+                self._display_rules()
                 game.add_marker()
                 break
 
@@ -45,7 +50,7 @@ class App:
                 self._display_help_screen()
 
             elif user_input.lower() == "q":
-                break
+                sys.exit()
 
             else:
-                print("FUCK OFF")
+                print("Exit")
--- game_core2.py
+++ game_core2.py
@@ -7,21 +7,23 @@ class GameCoreCopy:
 
     def __init__(self):
         self.board_state = np.zeros(shape=(3, 3))
-        self.i = 0
+        self.turn_counter = 0
 
     # def check_for_winning_conditions(self):
     #     pass
 
     def add_marker(self):
 
-        while self.i < 9:
+        while self.turn_counter < 9:
             response = (input())
-            x = int(response[1]) -1
-            print(x, type(x))
-            y = int(response[2]) - 1
+            row = int(response[1]) - 1
+            col = int(response[2]) - 1
             player = int(response[0])
-            self.board_state[x][y] = player
+            self.board_state[row][col] = player
             print(self.board_state)
-            self.i += 1
+            self.turn_counter += 1
 
         sys.exit()
+
+    def _play_board_status(self):
+        pass
